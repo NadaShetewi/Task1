781500d5e725df97dcc8a1038f5000d9de7f79d0
NadaShetewi <161207170+NadaShetewi@users.noreply.github.com>
2025-07-05 22:29:43 +0300

    print statements are done

--- src/Shipping/ShippingService.java
+++ src/Shipping/ShippingService.java
@@ -1,14 +1,21 @@
 package Shipping;
 import java.util.List;
 
+import Cart.CartItem;
 import Product.Shippable;
 
 public class ShippingService {
-    public static void ship(List<Shippable> items) {
-        System.out.println("Shipping items:");
-        for (Shippable item : items) {
-            System.out.printf(" - %s, weight: %.2fkg%n", item.getName(), item.getWeight());
+    public static void ship(List<CartItem> items) {
+        System.out.println("** Shipping notice **");
+         double totalWeight = 0;
+        for (CartItem item : items) {
+            int q = item.getQuantity();
+           double weight = ((Shippable)item.getProduct()).getWeight();
+           totalWeight += (q*weight);
+          System.out.printf(" - %d X  %s, weight: %.2fkg%n", item.getQuantity(), ((Shippable)item.getProduct()).getName(), ((Shippable)item.getProduct()).getWeight());
         }
+       System.out.printf("Total package weight %.2fkg%n", totalWeight);
+
     }
 }
 
--- src/System/ECommerceSystem.java
+++ src/System/ECommerceSystem.java
@@ -20,7 +20,7 @@ public class ECommerceSystem {
         if (cart.isEmpty()) throw new IllegalStateException("Cart is empty.");
 
         double subtotal = 0;
-        List<Shippable> itemsToShip = new ArrayList<>();
+        List<CartItem> itemsToShip = new ArrayList<>();
 
         for (CartItem item : cart.getItems()) {
             Product product = item.getProduct();
@@ -33,32 +33,39 @@ public class ECommerceSystem {
             subtotal += product.getPrice() * qty;
 
             if (product instanceof Shippable) {
-                itemsToShip.add((Shippable) product);
+                itemsToShip.add(item);
             }
         }
 
-        double shippingFees = itemsToShip.stream().mapToDouble(Shippable::getWeight).sum() * 2.5;//kilo price is 2.5
+        double shippingFees = 0.0;
+        double totalWeight = 0;
+        for(CartItem item: itemsToShip){
+           int q = item.getQuantity();
+           double weight = ((Shippable)item.getProduct()).getWeight();
+           totalWeight += (q*weight); 
+        }
+        shippingFees = totalWeight * 5;
         double total = subtotal + shippingFees;
 
         if (customer.getBalance() < total) {
             throw new IllegalStateException("Insufficient balance.");
         }
-
+        System.out.println("** Checkout receipt **");
         for (CartItem item : cart.getItems()) {
             item.getProduct().reduceQuantity(item.getQuantity());
+            System.out.printf(" - %d X  %s  %.2f%n", item.getQuantity(),item.getProduct().getName(), item.getProduct().getPrice());
         }
-
+        if (!itemsToShip.isEmpty()) {
+            ShippingService.ship(itemsToShip);
+        }
         customer.deduct(total);
-
-        
+         System.out.println("----------------------");
         System.out.printf("Subtotal: $%.2f%n", subtotal);
         System.out.printf("Shipping: $%.2f%n", shippingFees);
         System.out.printf("Total Paid: $%.2f%n", total);
         System.out.printf("Customer Remaining Balance: $%.2f%n", customer.getBalance());
 
-        if (!itemsToShip.isEmpty()) {
-            ShippingService.ship(itemsToShip);
-        }
+        
 
         cart.clear();
     }
